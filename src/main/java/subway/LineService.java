86fbe87ceb5835a0ee3fd883d2d4a2e30309a9b6
thddntjr1234 <thddntjr112233@gmail.com>
2024-07-09 22:40:21 +0900

    2단계 - 서비스 내의 createLineResponse()를 엔티티 내의 toResponse()로 리팩토링

--- src/main/java/subway/LineService.java
+++ src/main/java/subway/LineService.java
@@ -22,16 +22,12 @@ public class LineService {
 
         Line line = lineRepository.save(new Line(lineRequest.getName(), lineRequest.getColor(), upwardStation, downwardStation, lineRequest.getDistance()));
 
-        return createLineResponse(line);
+        return line.toResponse();
     }
 
     public List<LineResponse> findLines() {
         return lineRepository.findAll().stream()
                 .map(Line::toResponse)
                 .collect(Collectors.toList());
-    }
-
-    private LineResponse createLineResponse(Line line) {
-        return new LineResponse(line);
     }
 }
