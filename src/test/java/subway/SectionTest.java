d06c284d4fcce4b7373416fa3be4ceb41ea565e4
thddntjr1234 <thddntjr112233@gmail.com>
2024-07-14 20:47:25 +0900

    3단계 - 지하철 구간 삭제 단위 테스트 작성

--- src/test/java/subway/SectionTest.java
+++ src/test/java/subway/SectionTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("지하철 노선 구간 단위테스트")
@@ -18,8 +20,8 @@ public class SectionTest {
     @BeforeEach
     void setup() {
         //given
-        upStation = new Station("계양역");
-        downStation = new Station("국제업무지구역");
+        upStation = new Station(1L, "계양역");
+        downStation = new Station(2L, "국제업무지구역");
         Section section = new Section(upStation, downStation, 15);
         line = new Line("인천1호선", "bg-blue-400", section);
     }
@@ -41,7 +43,7 @@ public class SectionTest {
     void upStationFailure() {
         //when
         Station newUpStation = upStation;
-        Station newDownStation = new Station("송도달빛축제공원역");
+        Station newDownStation = new Station(3L, "송도달빛축제공원역");
         Section newSection = new Section(newUpStation, newDownStation, 3);
 
         //then
@@ -53,12 +55,31 @@ public class SectionTest {
     void successAdd() {
         //when
         Station newUpStation = downStation;
-        Station newDownStation = new Station("송도달빛축제공원역");
+        Station newDownStation = new Station(3L, "송도달빛축제공원역");
         Section newSection = new Section(newUpStation, newDownStation, 3);
 
         line.addSection(newSection);
 
         //then
         assertThat(line.getSections()).containsAnyOf(newSection);
+    }
+
+    @DisplayName("구간이 1개인 경우 역을 삭제할 수 없다.")
+    @Test
+    void deleteFailure() {
+        assertThatThrownBy(() -> line.deleteSection(3L)).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @DisplayName("정상 구간을 제거한다.")
+    @Test
+    void successDelete() {
+        //when
+        Station newUpStation = downStation;
+        Station newDownStation = new Station(3L, "송도달빛축제공원역");
+        Section newSection = new Section(newUpStation, newDownStation, 3);
+        line.addSection(newSection);
+
+        line.deleteSection(newDownStation.getId());
+        assertThat(line.getSections().get(0)).isEqualTo(new Section(upStation, downStation, 15));
     }
 }
